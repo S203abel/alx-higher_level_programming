e50e6f7834f2669cacf03911076fed4739738e26
S203abel <gebruabel7@gmail.com>
2023-12-08 13:19:16 -0800

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    i += 1
-    print(f"{i:02d}", end=", ")
+for i in range(99):
+    print("{:02d}".format(i), end=", ")
+    print("{:02d}".format(99))
+
