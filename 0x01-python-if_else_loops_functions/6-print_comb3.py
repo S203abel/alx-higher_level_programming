5b92be650efdab8b54a2b465796582ec9cbba71c
S203abel <gebruabel7@gmail.com>
2023-12-09 02:25:27 -0800

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(8):
+for i in range(10):
     for j in range(i+ 1, 10):
-        print(str(i) + str(j), end=", ")
-print(89)
+        if i == 8 and j == 9:
+            print('89')
+        else:
+            print(str(i) + str(j), end=", ")
